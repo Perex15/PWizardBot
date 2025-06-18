1780786ad7a102e395f5ad6b8872b24798841e84
Perex15 <149716430+Perex15@users.noreply.github.com>
2025-06-18 14:16:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQuery
 
 #Enable logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO) logger = logging.getLogger(name)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 ======================== BOT COMMANDS ==========================
 
